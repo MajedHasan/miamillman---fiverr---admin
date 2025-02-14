bc004ca9576a435e07e29998ded5c20657760080
Majed Hasan <mdmajedhasan599@gmail.com>
2025-02-15 03:44:05 +0600

    Ready for deployment

---
+++ src/authProvider.js
@@ -0,0 +1,45 @@
+const authProvider = {
+  login: async ({ username, password }) => {
+    const response = await fetch("http://localhost:5001/api/auth/login", {
+      method: "POST",
+      body: JSON.stringify({ email: username, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    const json = await response.json();
+    if (!response.ok || !json.jwt) {
+      throw new Error(json.message || "Login failed");
+    }
+
+    // ✅ Store token & user data in localStorage
+    localStorage.setItem(
+      "SyriaSouq-auth",
+      JSON.stringify({ token: json.jwt, user: json })
+    );
+  },
+
+  logout: () => {
+    localStorage.removeItem("SyriaSouq-auth");
+    return Promise.resolve();
+  },
+
+  checkAuth: () => {
+    const auth = JSON.parse(localStorage.getItem("SyriaSouq-auth"));
+    return auth ? Promise.resolve() : Promise.reject();
+  },
+
+  checkError: (error) => {
+    if (error.status === 401 || error.status === 403) {
+      localStorage.removeItem("SyriaSouq-auth");
+      return Promise.reject();
+    }
+    return Promise.resolve();
+  },
+
+  getPermissions: () => {
+    const auth = JSON.parse(localStorage.getItem("SyriaSouq-auth"));
+    return auth ? auth.user.role : "guest";
+  },
+};
+
+export default authProvider;
